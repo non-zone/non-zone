d1d42a28bf483e893d9c076455f43340d1b73ca1
rudensergey <rudensergey@gmail.com>
2020-06-14 07:28:16 +0300

    create object component

--- webapp/src/main_components/ObjectView.js
+++ webapp/src/main_components/ObjectView.js
@@ -1,6 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './object.css';
 
-// 
-export const renderObject = ({ item }) => {
-  return <div style={{ color: 'white' }}>TODO {item.title}</div>
-}
+const MapObject = (props) => {
+    const { title, description, onClick } = props;
+    const [state, setState] = useState(false);
+    return (
+        <div className="mapobject" onClick={() => setState(!state)}>
+            {state ? (
+                <>
+                    <div className="mapobject__box mapobject__animation">
+                        <div className="mapobject__header">
+                            <h5>{title}</h5>
+                            <p>{description}</p>
+                        </div>
+                        <button
+                            className="mapobject__button"
+                            onClick={() => onClick()}
+                        >
+                            open this nonzone
+                        </button>
+                    </div>
+                </>
+            ) : (
+                ''
+            )}
+        </div>
+    );
+};
+
+export const renderObject = ({ item: { title, description } }) => {
+    return (
+        <MapObject
+            title={title}
+            description={description}
+            onClick={() => alert('it works')}
+        />
+    );
+};
