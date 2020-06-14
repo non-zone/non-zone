9102021d8418ff7af4801005c1e7f8637da50d13
Dmitry Yudakov <dmitry.yudakov@gmail.com>
2020-06-14 16:11:35 +0300

    Dummy image is shown instead of profile, when profile is existing

--- webapp/src/components/image/image.js
+++ webapp/src/components/image/image.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
 import './image.css';
 
-export const Image = (props) => {
+export const Image = ({ onClose }) => {
     const [display, setDisplay] = useState(true);
     return (
         <>
             {display ? (
                 <div className="image">
                     <div
-                        onClick={() => setDisplay(false)}
+                        onClick={() => {
+                            setDisplay(false);
+                            onClose && onClose();
+                        }}
                         className="image__pic splash-appearance"
                     ></div>
                 </div>
