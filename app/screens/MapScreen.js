91d14af8aad18a137af06f0578a5e07319f19286
bengyles <bengyles@hotmail.com>
2020-08-04 20:31:25 +0200

    Added maps with custom marker for a location I like

--- app/screens/MapScreen.js
+++ app/screens/MapScreen.js
@@ -1,24 +1,54 @@
-import * as React from 'react';
-import { StyleSheet, Text} from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import React, {Component} from 'react';
+import {StyleSheet, View, Dimensions} from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
 
-export default function ShowStoryScreen() {
-  return (
-    <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-      <Text>Map screen</Text>
-    </ScrollView>
-  );
+class MapScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markers: [
+        {
+          coordinate: {
+            latitude: 51.2992875,
+            longitude: 4.6915887,
+          },
+          title: 'Jeugdhuis Babylon',
+          description: 'A great and fairly cheap place to have a drink and chill with the youth of the town of Westmalle',
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+        <View style={styles.container}>
+          <MapView style={styles.mapStyle} minZoomLevel={4}>
+            {this.state.markers.map(marker => (
+                <Marker
+                    key={marker}
+                    coordinate={marker.coordinate}
+                    title={marker.title}
+                    description={marker.description}
+                    image={require('../assets/images/marker.png')}
+                />
+            ))}
+          </MapView>
+        </View>
+    );
+  }
 }
+
+export default MapScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fafafa',
-  },
-  contentContainer: {
-    flex: 1,
-    paddingTop: 15,
+    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
