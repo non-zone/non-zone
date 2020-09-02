f39d0a620e9529c0b1211e7912280790c84c1c2d
bengyles <bengyles@hotmail.com>
2020-09-02 19:22:24 +0200

    initialize user location on the map

--- app/screens/MapScreen.js
+++ app/screens/MapScreen.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, Dimensions } from 'react-native';
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import mapStyle from '../constants/mapStyle';
 import { useLoadStories } from '../services/db';
+import * as Location from 'expo-location';
+
+const { width, height } = Dimensions.get('window');
+const SCREEN_HEIGHT = height;
+const SCREEN_WIDTH = width;
+const ASPECT_RATIO = width / height;
+const LATITUDE_DELTA = 10;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 export const MapScreen = (props) => {
+    let [location, setLocation] = useState({
+        latitude: 0,
+        longitude: 0,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+    });
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestPermissionsAsync();
+            if (status !== 'granted') {
+                console.log('location not granted');
+            }
+
+            let position = await Location.getCurrentPositionAsync({});
+            console.log(position);
+            setLocation({
+                latitudeDelta: LATITUDE_DELTA,
+                longitudeDelta: LONGITUDE_DELTA,
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+            });
+        })();
+    }, []);
     const { error, loading, data = [] } = useLoadStories();
     // console.log({ error, loading, data });
 
@@ -12,7 +44,9 @@ export const MapScreen = (props) => {
         <View style={styles.container}>
             <MapView
                 style={styles.mapStyle}
+                region={location}
                 minZoomLevel={2}
+                maxZoomLevel={18}
                 customMapStyle={mapStyle}
                 provider={PROVIDER_GOOGLE}
                 onLongPress={(mapEvent) => {
@@ -48,7 +82,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     mapStyle: {
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height,
+        width: SCREEN_WIDTH,
+        height: SCREEN_HEIGHT,
     },
 });
