df0099408c74f73d402457e5080a2a04e41708e1
bengyles <bengyles@hotmail.com>
2020-10-28 19:03:08 +0100

    Fix tipping yourself issue and removed margin

--- webapp/src/main_components/Nonzone.js
+++ webapp/src/main_components/Nonzone.js
@@ -138,7 +138,9 @@ export const Nonzone = ({ onClose }) => {
     const onLeaveComment = async (comment) => {
         try {
             await leaveComment(object.id, comment);
-            setTippingModalVisible(true);
+            if (object.uid != user.uid) {
+                setTippingModalVisible(true);
+            }
         } catch (err) {
             alert(err.toString());
         }
